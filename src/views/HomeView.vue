9cf1f1bff1445b0d3f2f19df0690e42334c88adb
Chris <he1237596@163.com>
2024-08-13 23:51:56 +0800

    feat: 添加pinia

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -2,9 +2,16 @@
  * @Author: Chris
  * @Date: 2024-08-13 19:53:54
  * @LastEditors: Chris
- * @LastEditTime: 2024-08-13 23:20:40
+ * @LastEditTime: 2024-08-13 23:49:20
  * @Descripttion: **
 -->
+<script setup>
+import { useCounterStore } from '@/stores'
+const userStore = useCounterStore()
+</script>
+
 <template>
-  <main>这是home页面</main>
+  <h1>这是home页面</h1>
+  {{ userStore.count }}
+  <button @click="userStore.increment">+</button>
 </template>
