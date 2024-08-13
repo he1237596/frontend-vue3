65c4424e157f396cafd5a9bb41d42f6aaa978e93
Chris <he1237596@163.com>
2024-08-13 23:22:57 +0800

    test: hysky-lint

---
+++ src/router/index.js
@@ -0,0 +1,30 @@
+/*
+ * @Author: Chris
+ * @Date: 2024-08-13 19:53:54
+ * @LastEditors: Chris
+ * @LastEditTime: 2024-08-13 21:22:57
+ * @Descripttion: **
+ */
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/HomeView.vue'
+
+const router = createRouter({
+  history: createWebHistory(import.meta.env.BASE_URL),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue')
+    }
+  ]
+})
+
+export default router
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<!--
+ * @Author: Chris
+ * @Date: 2024-08-13 19:53:54
+ * @LastEditors: Chris
+ * @LastEditTime: 2024-08-13 23:20:40
+ * @Descripttion: **
+-->
+<template>
+  <main>这是home页面</main>
+</template>
